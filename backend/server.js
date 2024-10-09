12f3e460c15ee951a92fecc662ff530fa66ba5fe
Tiarajuby <155611699+Tiarajuby@users.noreply.github.com>
2024-10-09 21:26:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,27 +12,10 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;  // Use the port from Render in production
 
-// Set allowed origins
-const allowedOrigins = [
-    process.env.CLIENT_URL, // For production
-    'http://localhost:3000', // For local development
-    'https://zepto-1-smnp.vercel.app','https://zepto-1-vvvu.vercel.app/' // Your deployed frontend URL
-];
+
 
 // Middleware
-app.use(cors({
-    origin: function (origin, callback) {
-        // Allow requests with no origin (like mobile apps, curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify allowed HTTP methods
-    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-}));
+app.use(cors());
 
 app.use(express.json());
 
