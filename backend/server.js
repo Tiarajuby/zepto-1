5c3673f1b73fc260e7e04fae1b63825836a06de3
Tiarajuby <155611699+Tiarajuby@users.noreply.github.com>
2024-10-10 02:15:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,12 +13,30 @@ const app = express();
 const port = process.env.PORT || 4000;  // Use the port from Render in production
 
 // Middleware
-app.use(cors({
-    origin: 'https://zepto-1-2akz.vercel.app', // Set to your frontend domain
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-}));
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+
+module.exports = allowCors(handler)
+
 
 app.use(express.json());
 
