173899911382bf6a9315fe21754e2da64b059c60
Tiarajuby <155611699+Tiarajuby@users.noreply.github.com>
2024-10-11 01:13:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,30 +13,15 @@ const app = express();
 const port = process.env.PORT || 4000;  // Use the port from Render in production
 
 // Middleware
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
+const cors = require('cors');
 
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
-}
+const corsOptions = {
+    origin: 'https://zepto-1-gykb.vercel.app', // allow your Vercel frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+};
 
-module.exports = allowCors(handler)
-
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
