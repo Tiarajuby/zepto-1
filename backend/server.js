7d16aa7da5e9433aaa17735354776fcf772c6fe2
Tiarajuby <155611699+Tiarajuby@users.noreply.github.com>
2024-10-14 11:13:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,37 @@
-import express from 'express';
-import cors from 'cors';
-import { connectDB } from './config/db.js';
-import foodRouter from './routes/foodRoute.js';
+import express from 'express'
+import cors from 'cors'
+import { connectDB } from './config/db.js'
+import foodRouter from './routes/foodRoute.js'
 import userRouter from './routes/userRoute.js';
+import 'dotenv/config';
 import cartRouter from './routes/cartRoute.js';
 import orderRouter from './routes/orderRoute.js';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
-const app = express();
-const port = process.env.PORT || 4000;  // Use the port from Render in production
 
-// Middleware
-const cors = require('cors');
+//app config
+const app = express()
+const port = 4000
 
-const corsOptions = {
-    origin: 'https://zepto-1-gykb.vercel.app', // allow your Vercel frontend
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-};
+// middleware
+app.use(express.json())
+app.use(cors())
 
-app.use(cors(corsOptions));
-
-app.use(express.json());
-
-// Database connection
+//db connection
 connectDB();
 
-// API endpoints
-app.use("/api/food", foodRouter);
-app.use("/images", express.static('uploads'));
-app.use('/api/user', userRouter);
-app.use('/api/cart', cartRouter);
-app.use('/api/order', orderRouter);
+// api endpoints
+app.use("/api/food",foodRouter)
+app.use("/images",express.static('uploads'))
+app.use('/api/user', userRouter)
+app.use('/api/cart', cartRouter)
+app.use('/api/order', orderRouter)
 
-app.get("/", (req, res) => {
-    res.send("API working");
-});
+app.get("/",(req,res)=>{
+        res.send("API working")
+})
 
-// Start server
-app.listen(port, () => {
-    console.log(`Server started on http://localhost:${port}`);
-});
+app.listen(port,()=>{
+    console.log(`Server started on http://localhost:${port}`)
+})
